39df371d8dc30819b89b351620828eb755505752
Stefanos Carlström <stefanos.carlstrom@gmail.com>
2020-02-18 16:56:35 +0100

    Initial functionality

--- src/UniformScalingMatrixFunctions.jl
+++ src/UniformScalingMatrixFunctions.jl
@@ -1,5 +1,25 @@
 module UniformScalingMatrixFunctions
+using LinearAlgebra
 
-greet() = print("Hello World!")
+# Matrix/operator functions f(A) are defined as either the Taylor
+# series of f evaluated using the matrix A, or equivalently, as f(A) =
+# Q*f(λ)*Qᴴ, where Q is the matrix of eigenvectors of A and λ the
+# associated eigenvalues. UniformScaling represents linear operators
+# of the form α*I, where α is a scalar, which simultaneously is the
+# only eigenvalue. A matrix/operator function f(α*I) is thus simply
+# f(α)*I, which is also valid from the Taylor series perspective,
+# since I^k==I.
+for f in [:exp,
+          :sin, :cos, :tan,
+          :asin, :acos, :atan,
+          :sinh, :cosh, :tanh,
+          :asinh, :acosh, :atanh,
+          :log, :sqrt]
+    @eval begin
+        Base.$f(J::UniformScaling) = UniformScaling($(f)(J.λ))
+    end
+end
+
+export I
 
 end # module
--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,26 @@ using UniformScalingMatrixFunctions
 using Test
 
 @testset "UniformScalingMatrixFunctions.jl" begin
-    # Write your own tests here.
+    @test exp(0I) === 1.0I
+    @test exp(-im*π/2*I) ≈ -im*I
+    @test exp(-im*π/4*I) ≈ (1-im)/√(2)*I
+
+    @test sin(0I) === 0.0I # This assumes iszero(sin(0)), always true?
+    @test sin(π/2*I) ≈ I
+    @test cos(0I) === 1.0I # This assumes isone(cos(0)), always true?
+    @test cos(π/2*I) ≈ 0I atol=1e-14
+    @test tan(π/4*I) ≈ I
+
+    @test asin(I) ≈ π/2*I
+    @test acos(I) ≈ 0*I atol=1e-14
+    @test atan(I) ≈ π/4*I
+
+    # Test hyperbolics + inverses
+
+    @test log(I) === 0.0I
+    @test log(exp(I)) === 1.0I
+
+    @test √(4I) === 2.0I
+    @test √(4I) == 2I
+    @test √(4.0I) === 2.0I
 end
